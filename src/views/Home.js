e3903823793b5be3e18ae915637156fa38dd6d44
Natanael <natanael_magalhaes@msn.com>
2019-12-15 15:14:41 -0300

    home route

---
+++ src/views/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ModelsApi from '../api/ModelsApi'
+
+function Home() {
+  return (
+    <div className="container mt-5">
+      <div className="jumbotron">
+        <h1>Fitsbook</h1>
+        <p>Homepage</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
