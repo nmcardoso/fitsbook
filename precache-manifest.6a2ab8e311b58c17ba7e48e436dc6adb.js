2501defa39140249aa7e2620d4f7edcc4fe89e57
GH Pages Deploy <deploy@example.com>
2019-12-19 16:36:32 +0000

    Updates

---
+++ precache-manifest.6a2ab8e311b58c17ba7e48e436dc6adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59781566875d97ae71163702a2a62f2e",
+    "url": "/fitsbook/index.html"
+  },
+  {
+    "revision": "4876214cdc3c34cd6834",
+    "url": "/fitsbook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59a76651ed4730171132",
+    "url": "/fitsbook/static/js/2.970bd551.chunk.js"
+  },
+  {
+    "revision": "6bca273eaa400b752af73c506d30bc4b",
+    "url": "/fitsbook/static/js/2.970bd551.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4876214cdc3c34cd6834",
+    "url": "/fitsbook/static/js/main.678e66f0.chunk.js"
+  },
+  {
+    "revision": "728bab8b36f4558aab63",
+    "url": "/fitsbook/static/js/runtime-main.b6f11cd7.js"
+  }
+]);
