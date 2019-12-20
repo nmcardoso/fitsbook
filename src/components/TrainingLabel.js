0be5d2689bf0336790ffe478c0bf8298bddecddf
Natanael <natanael_magalhaes@msn.com>
2019-12-20 12:14:56 -0300

    training label with bootstrap grow spinner

---
+++ src/components/TrainingLabel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TrainingLabel extends React.Component {
+  render() {
+    return (
+      <div>
+        <span className="spinner-grow spinner-grow-sm text-success"></span>
+        <span className="text-success ml-1">Training</span>
+      </div>
+    );
+  }
+}
+
+export default TrainingLabel;
