cfecf8cee495b2d6d36f6ad68f260baa51a2486f
Natanael <natanael_magalhaes@msn.com>
2019-12-22 13:44:17 -0300

    css spinner animation

---
+++ src/components/Spinner.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Spinner.css';
+
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    }
+    this.intervalId = setInterval(() => {
+      this.setState({ show: true });
+    }, 200);
+  }
+
+  componentWillUnmount() {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  render() {
+    return (
+      this.state.show && <div className="spinner"></div>
+    );
+  }
+}
+
+export default Spinner;
