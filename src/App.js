534781d96779f2497fe3442b96ee7a92e848c874
Natanael <natanael_magalhaes@msn.com>
2020-01-05 01:04:49 -0300

    Login route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomeRoute from './components/HomeRoute';
 import ModelsRoute from './components/ModelsRoute';
 import StatsRoute from './components/StatsRoute';
+import LoginRoute from './components/LoginRoute';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' component={HomeRoute} />
         <Route path='/models' component={ModelsRoute} />
         <Route path='/stats/:id' component={StatsRoute} />
+        <Route path="/login" component={LoginRoute} />
       </Switch>
     </div>
   );
