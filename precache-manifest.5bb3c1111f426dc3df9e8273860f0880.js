f49b3075e74d54755cd68db8bfb0c075f16002b6
GH Pages Deploy <deploy@example.com>
2021-01-09 00:01:57 +0000

    Updates

---
+++ precache-manifest.5bb3c1111f426dc3df9e8273860f0880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77a241aa4aab990757b6d19146bb776",
+    "url": "/fitsbook/index.html"
+  },
+  {
+    "revision": "04c3f04650fbdcff8c76",
+    "url": "/fitsbook/static/css/main.64adca46.chunk.css"
+  },
+  {
+    "revision": "5bf46054e6574329fa1c",
+    "url": "/fitsbook/static/js/2.cd45ec45.chunk.js"
+  },
+  {
+    "revision": "405d4dfbed6106c62a84f7e1bb68595f",
+    "url": "/fitsbook/static/js/2.cd45ec45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c3f04650fbdcff8c76",
+    "url": "/fitsbook/static/js/main.f3e724ce.chunk.js"
+  },
+  {
+    "revision": "78480c90f10e04201ba0",
+    "url": "/fitsbook/static/js/runtime-main.775cf828.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e9acab47c643b849cc24f0dde12f8f9.js"
+  "/fitsbook/precache-manifest.5bb3c1111f426dc3df9e8273860f0880.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fitsbook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
